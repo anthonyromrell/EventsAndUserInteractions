a608d8d0e2f526006df826dc6d074ad689eb437a
Anthony Romrell <anthony@artisandream.com>
2018-08-29 14:06:35 -0600

    Added UnityEvents

---
+++ DestroyEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//Made By Anthony Romrell
+public class DestroyEvent : MonoBehaviour
+{
+	public UnityEvent Event;
+
+	private void OnDestroy()
+	{
+		Event.Invoke();
+	}
+}
